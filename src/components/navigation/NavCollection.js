8a3543c97c2626e7b0ca2d5cd15c7c25e1b64f38
Blake <blakemcd@gmail.com>
2021-12-03 21:40:20 +1100

    got first api request working

--- src/components/navigation/NavCollection.js
+++ src/components/navigation/NavCollection.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
+import ItemContainer from '../ItemContainer' 
 
 function NavCollection() {
+
+    const url = 'http://localhost:3000/api/v1/products'
+
+    const [products, setProducts] = useState([]);
+
+    useEffect( async() => {
+        //fetch data
+        const response = await fetch(url);
+        const data = await response.json();
+
+        //send data to state
+        setProducts(data)
+    }, [])
+
+    const renderProducts = () => {
+        if (products.length === 0) {
+            return (
+                <p>Products Loading</p>
+            )
+        }
+        else {
+            return (
+                <div className="collectionContainer">
+                    {
+                        products.map( (product) => {
+                            return (
+                                <>
+                                    <p>item should go here</p>
+                                    <ItemContainer
+                                        key={product.id}
+                                        prod_name={product.name}
+                                    />
+                                </>
+                            )
+                        })
+                    }
+                </div>  
+            )
+        }
+    }
+
     return (
         <>
             <Header/>
-
+            {renderProducts()}
             <Footer/>
         </>
     )
