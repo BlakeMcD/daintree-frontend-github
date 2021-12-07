d05f0edbefc9b925688c080b91b2851f0c7b8b74
Blake <blakemcd@gmail.com>
2021-12-07 11:24:19 +1100

    unsuccessful so far - set up the request but no response

--- src/components/navigation/NavLoginContent.js
+++ src/components/navigation/NavLoginContent.js
@@ -7,6 +7,9 @@ function NavLoginContent() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    //test
+    const [dadata, setDadata] = useState(["blank"])
+
     const handleChangeEmail = (event) => {
         setEmail(event.target.value)
     }
@@ -14,12 +17,61 @@ function NavLoginContent() {
     const handleChangePassword = (event) => {
         setPassword(event.target.value)
     }
+    
+    const handleSubmitGet = (event) => {
+
+        const posturl2 = 'http://localhost:3000/api/v1/login';
+
+        const requestOptions2 = {
+            method: 'GET', 
+            url: posturl2,
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({ 
+                email: {email},
+                password: {password}
+            })
+        };
+
+        fetch(posturl2, requestOptions2)
+            .then(response => response.json())
+            .then(data2 => {
+                console.log("did this work??")
+                setDadata(data2)})
+
+        
+    }
+
+    const handleSubmit = (event) => {
+        alert('Request sent');
+        // event.preventDefault();
+
+        const posturl = 'http://localhost:3000/api/v1/login';
+
+        const requestOptions = {
+            method: 'POST',
+            url: posturl,  
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({ 
+                email: {email},
+                password: {password}
+            })
+        };
+
+        fetch(posturl, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                setDadata(data)})
+       
+    }
+
+    
 
     return (
         <>
             <Header/>
             <h1> Nav Login Content Content Page</h1>
-            <form>
+            <h2> Data: {dadata} </h2>
+            <form onSubmit={handleSubmitGet}>
                 <label>
                     Email Address:
                     <input type="text" value={email} onChange={handleChangeEmail}/>
@@ -28,6 +80,7 @@ function NavLoginContent() {
                     Password:
                     <input type="password" value={password} onChange={handleChangePassword}/>
                 </label>
+                <input type="submit" value="Submit" />
             </form>
             <Footer/>
         </>
