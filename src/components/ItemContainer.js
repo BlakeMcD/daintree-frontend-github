a2d659e80cebafb48fc5c073f48b10530109d5fd
Blake <blakemcd@gmail.com>
2021-12-09 17:42:42 +1100

    got frontend working with new backend

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -11,12 +11,12 @@ function ItemContainer(props) {
         <div className="item">
 
             <div className="item__images">
-                <img className="item__images__itemImage" src={props.prod_images[0].img_url} alt="Item Image"/>
+                <img className="item__images__itemImage" src={props.prod_image} alt="Item Image"/>
                 <img className="item__images__itemStoreLogo" src={props.prod_store_logo}  alt="Store Logo"/>
             </div>
             <p className="item__title">{props.prod_name}</p>
             <p className="item__price">{priceWithFormatting}</p>
-            <p className="item__stocks">{props.prod_stocks[0].size}</p>
+            <p className="item__stocks">{props.prod_size}</p>
             <AddToCart/> 
         </div>
     )
