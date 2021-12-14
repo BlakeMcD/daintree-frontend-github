02c71f32c62ff6b47b922f37a2ae6c3867d3ce01
Blake <blakemcd@gmail.com>
2021-12-14 12:34:39 +1100

    made progress with slideshow

--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -6,6 +6,7 @@
  import MostWantedContainer from '../MostWantedContainer'
  import HomepageWoman01 from '../../images/HomepageWoman01.png'
  import HomepageMan01 from '../../images/HomepageMan01.png'
+ import ShopSlideshow from '../ShopSlideshow'
 
  
  function Home() {
@@ -18,7 +19,7 @@
                 <HomePageImage imageSource={HomepageWoman01} imageAlt="Shop Women's Collection Image" buttonText={"WOMEN"}/>
             </div>
             <h2>Featured Stores</h2>
-            <StoreContainer/>
+            <ShopSlideshow/>
             <h2>Most Wanted</h2>
              <MostWantedContainer/>
              <div className="spacer"/>
