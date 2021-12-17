503bfb4da0000b5001a369e38701abff9a1936b9
Blake <blakemcd@gmail.com>
2021-12-17 17:00:43 +1100

    added login text to login button

--- src/components/navigation/NavLogin.js
+++ src/components/navigation/NavLogin.js
@@ -6,20 +6,24 @@ import { Link } from 'react-router-dom'
 function NavLogin() {
 
     const [img, setImg] = useState(UserSigninWhite)
+    const [text, setText] = useState("white")
 
     return (
         <>
             <Link to={"/login"}>
-                <img 
-                    alt="Sign In"
-                    src={img} 
-                    onMouseEnter={() => {
-                        setImg(UserSigninTan)
-                    }}
-                    onMouseOut={() => {
-                        setImg(UserSigninWhite)
-                    }}
-                ></img>
+                    <img 
+                        alt="Sign In"
+                        src={img} 
+                        onMouseEnter={() => {
+                            setImg(UserSigninTan)
+                            setText("#F0EACE")
+                        }}
+                        onMouseOut={() => {
+                            setImg(UserSigninWhite)
+                            setText("white")
+                        }}
+                    ></img>
+                    <span style={{color:text}}>LOGIN</span>
             </Link>
         </>
     )
